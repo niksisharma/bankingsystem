d6f41f7eb97a68e148ed8003f4ce58da1dcd9494
niksisharma <51536421+niksisharma@users.noreply.github.com>
2021-07-21 19:06:03 +0530

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$user = 'id17284631_db';
+$pass = 'EpWPa)M@(8z)NUJ6';
+$server = 'localhost';
+$db  = 'id17284631_bankingsystem';
+
+$conn = mysqli_connect($server, $user, $pass, $db) or die("No connection ".mysqli_connect_error());
+
+?>
