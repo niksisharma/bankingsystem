bcd58acb2782e7ec89a0ecea82700df4f6634a8f
niksisharma <51536421+niksisharma@users.noreply.github.com>
2021-07-21 19:07:05 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sparks Foundation Bank</title>
+        <?php include 'links.php' ?> 
+        
+    </head>
+    <body>
+        <?php include 'nav.php' ?>
+        <h1>Welcome to Sparks Foundation Bank</h1>
+        <div class="view_cust">
+            <table>
+            <tr><td><a class="btn btn-secondary cbtn" href="users.php" role="button">View All Customers</a></td>
+            <td>
+            <a class="btn btn-secondary cbtn" href="history.php" role="button">View Transaction History</a></td>
+            </tr>
+            </table>
+        </div>        
+    </body>
+</html>
